4cd2f7c791787fe30b4bdee5cc6d93fc010dd0e6
Sanya Kravchuk <starkemc@gmail.com>
2021-04-11 21:47:46 +0300

    fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
   env: {
-    es6: true,
     node: true,
   },
   extends: 'airbnb/base',
   plugins: ['prettier'],
   parserOptions: {
-    ecmaVersion: 6,
+    ecmaVersion: 8,
   },
   rules: {
     'prettier/prettier': ['error'],

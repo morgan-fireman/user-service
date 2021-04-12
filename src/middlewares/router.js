f9310ad31dd447a311411283c6ce55516129e515
Sanya Kravchuk <starkemc@gmail.com>
2021-04-12 22:20:13 +0300

    refactored code

--- src/middlewares/router.js
+++ src/middlewares/router.js
@@ -1,20 +1,10 @@
-const db = require('../db');
-
-const responseResolver = async (ctx, path) => {
-  try {
-    const data = await db.getData(path);
-    ctx.response.type = 'application/json';
-    ctx.response.body = data;
-  } catch (err) {
-    ctx.response.status = 500;
-  }
-};
+const utils = require('./utils');
 
 const router = (path) => async (ctx, next) => {
   const { method, url } = ctx.request;
 
   if (method === 'GET' && (url === path || url === `${path}/`)) {
-    await responseResolver(ctx, path);
+    await utils.responseResolver(ctx, path);
   } else {
     ctx.response.status = 404;
   }
---
+++ src/middlewares/utils.js
@@ -0,0 +1,15 @@
+const db = require('../db');
+
+const responseResolver = async (ctx, path) => {
+  try {
+    const data = await db.getData(path);
+    ctx.response.type = 'application/json';
+    ctx.response.body = data;
+  } catch (err) {
+    ctx.response.status = 500;
+  }
+};
+
+module.exports = {
+  responseResolver,
+};

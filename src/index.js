4101d283cd39670e86bf991acb5e013c46430782
morgan-freeman-ls <morgan.freeman@zohomail.eu>
2021-04-13 20:42:52 +0000

    [NODE-USER-SRV] changing script using eslint option

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const Koa = require("koa");
 const logger = require("koa-logger");
-const bodyParser = require("koa-bodyparser');
-    const app = new Koa();
-    const port = 30000;
+const bodyParser = require("koa-bodyparser");
 
-    // adding logging
-    app.use(logger()); app.use(bodyParser());
+const app = new Koa();
+const port = 30000;
 
-    app.use(async ctx => {
-      ctx.body = 'Hello World';
-    });
+// adding logging
+app.use(logger());
+app.use(bodyParser());
 
-    app.listen(port);
+app.use(async (ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(port);

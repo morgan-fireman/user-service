9277b25ee8686d6937ffcc17060b935dccefcbf6
morgan-freeman-ls <morgan.freeman@zohomail.eu>
2021-04-13 20:39:58 +0000

    [NODE-USER-SRV] server script fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const logger = require("koa-logger");
-const bodyParser = require('koa-bodyparser');
-const app = new Koa();
-const port = 30000;
+const bodyParser = require("koa-bodyparser');
+    const app = new Koa();
+    const port = 30000;
 
-// adding logging
-app.use(logger());
-app.use(bodyParser());
+    // adding logging
+    app.use(logger()); app.use(bodyParser());
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+    app.use(async ctx => {
+      ctx.body = 'Hello World';
+    });
 
-app.listen(port);
+    app.listen(port);

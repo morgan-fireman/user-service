b4653b23204874842ab3fd8519871b159bf5cb43
morgan-freeman-ls <morgan.freeman@zohomail.eu>
2021-04-13 20:14:26 +0000

    [NODE-USER-SRV] basic koa-app added

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const logger = require("koa-logger");
+const bodyParser = require('koa-bodyparser');
+const app = new Koa();
+const port = 30000;
+
+// adding logging
+app.use(logger());
+app.use(bodyParser());
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(port);
